c38cfc76dc7559a9f178746f245f1952a83da36d
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2024-07-04 15:22:12 +0900

    [2장] API - ResponseBody 문자 반환과 객체 반환 실습

--- src/main/java/hello/hello_spring/HelloController.java
+++ src/main/java/hello/hello_spring/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -22,5 +23,31 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name_value, Model model) {
         model.addAttribute("name", name_value);
         return "hello-mvc";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name_value) {
+        return "안녕하세요. " + name_value + "님.";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name_value) {
+        Hello hello = new Hello();
+        hello.setName(name_value);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
