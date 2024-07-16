2a555a3c31ba12b8cefb71c7c4c29df2037a4db4
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2024-07-16 14:31:30 +0900

    [6장] 스프링 데이터 JPA

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -11,12 +11,21 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+    */
 
     /*
     private DataSource dataSource;
@@ -29,9 +38,10 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+    /*
     @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
@@ -39,4 +49,5 @@ public class SpringConfig {
         // return new JdbcTemplateMemberRepository(dataSource);
         return new JpaMemberRepository(em);
     }
+    */
 }
